59a72b764cf4f726ddef24138879d349dd36a06d
Tatyana Pashkovich <tatyana.pashkovich@gmail.com>
2024-03-11 14:11:34 -0400

    [dev] added the demo

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import Vault from "./components/Vault.vue";
+import { getData } from "./data";
+
+export default {
+  components: { Vault },
+  data() {
+    return { data: getData() };
+  },
+};
+</script>
+
+<template>
+  <Vault :data="data" />
+</template>
